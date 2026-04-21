47564aa61b4baf5062b075a7470a91a99196027e
friera1 <friera145@mail.ru>
2026-04-21 12:17:42 +0500

    Initial bootstrap: add UI theme constants

---
+++ scripts/ui/UITheme.gd
@@ -0,0 +1,14 @@
+extends Node
+
+const COLOR_BG = Color("0B120F")
+const COLOR_PANEL = Color("122019")
+const COLOR_CARD = Color("173026")
+const COLOR_JADE = Color("2FAF7F")
+const COLOR_JADE_LIGHT = Color("4FBF9F")
+const COLOR_GOLD = Color("D8B66A")
+const COLOR_GOLD_DARK = Color("9E7B37")
+const COLOR_TEXT = Color("F2E9D8")
+const COLOR_TEXT_SECONDARY = Color("CDBF9F")
+const COLOR_MUTED = Color("7D7A6E")
+const COLOR_SUCCESS = Color("56C778")
+const COLOR_DANGER = Color("C24C3A")
